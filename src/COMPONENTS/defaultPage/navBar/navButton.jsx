2e264d0e331f7bc5e881e5a52f9cb42c7a4784d9
Robin Ryan <robin1ryan@hotmail.com>
2025-04-08 12:21:23 +0200

    added styling to buttons. Going to fix them and tidy them up now

--- src/COMPONENTS/defaultPage/navBar/navButton.jsx
+++ src/COMPONENTS/defaultPage/navBar/navButton.jsx
@@ -1,9 +1,39 @@
 import classes from "./navArea.module.scss";
+import { useRef, useEffect } from "react";
 
 const NavButton = ({ children }) => {
+  const btnRef = useRef(null);
+  const spanRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { width, height } = e.target.getBoundingClientRect();
+
+      const offsetX = e.offsetX;
+      const offsetY = e.offsetY;
+
+      const left = `${(offsetX / width) * 100}%`;
+      const top = `${(offsetY / height) * 100}%`;
+
+      spanRef.current.animate(
+        { left, top },
+        { duration: 250, fill: "forwards" }
+      );
+    };
+
+    btnRef.current.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      btnRef.current?.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className={classes.navButtonContainer}>
-      <button>{children}</button>
+      <button ref={btnRef}>
+        <div className={classes.insideButtonWrapper}>{children}</div>
+        <span className={classes.mouseDot} ref={spanRef} />
+      </button>
     </div>
   );
 };
