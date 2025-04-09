aec1d02d7cbbf21a0b2a80a61bd263a616bad989
Robin Ryan <robin1ryan@hotmail.com>
2025-04-09 18:59:45 +0200

    made it so on-load the correct button is active

--- src/COMPONENTS/defaultPage/navBar/navBar.jsx
+++ src/COMPONENTS/defaultPage/navBar/navBar.jsx
@@ -1,13 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import classes from "./navArea.module.scss";
 import NavButton from "./navButton";
 import NavTitle from "./navTitle";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchInput from "../searchInput/searchInput";
 
 const NavBar = () => {
-  const [activeButton, setActiveButton] = useState(0);
+  const [activeButton, setActiveButton] = useState(null);
   const navigate = useNavigate();
+  const { home, name } = useParams();
+
+  useEffect(() => {
+    for (let i = 0; i < Object.keys(buttonsObject).length; i++) {
+      if (`home/${name}` === Object.values(buttonsObject)[i]) {
+        setActiveButton(i);
+      }
+      if (!name) {
+        setActiveButton(0);
+      }
+    }
+  }, []);
 
   const handleNavigate = (link) => {
     navigate(`/${link}/`);
