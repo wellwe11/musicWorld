c8f31ed8f20ede03a4e06ee5f3369bf2fd6bf6db
Robin Ryan <robin1ryan@hotmail.com>
2025-04-10 21:04:40 +0200

    added transition to each event through ref

--- src/COMPONENTS/upcomingEventsPage/eventComp.jsx
+++ src/COMPONENTS/upcomingEventsPage/eventComp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import classes from "./upcomingEvents.module.scss";
 
 const Event = ({ title, date, image, location }) => {
--- src/COMPONENTS/upcomingEventsPage/events.jsx
+++ src/COMPONENTS/upcomingEventsPage/events.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import Event from "./eventComp";
 
 import classes from "./upcomingEvents.module.scss";
@@ -6,12 +6,39 @@ import { EventContext } from "../../App";
 
 const Events = () => {
   const { events, loading } = useContext(EventContext);
+  // ref for effect below
+  const elementsRef = useRef([]);
+
+  // creates a smooth transition for events to make them look a bit
+  // nicer when scrolling
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("show");
+          entry.target.classList.add(classes.show);
+        } else {
+          entry.target.classList.remove(classes.show);
+        }
+      });
+    });
+
+    elementsRef.current.forEach((el) => {
+      if (el) observer.observe(el);
+    });
+
+    return () => observer.disconnect();
+  });
 
   return (
     <div className={classes.events}>
       {!loading &&
         events.map((event, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            ref={(el) => (elementsRef.current[index] = el)}
+            className={classes.eventContainer}
+          >
             <Event
               title={event.name}
               date={event.dates.start.dateTime}
