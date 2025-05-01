4a43785be272b58d7840726e28958945b4f26dc5
Robin Ryan <robin1ryan@hotmail.com>
2025-05-01 11:06:21 +0200

    minor updates

--- src/COMPONENTS/homeComponents/homeComponent.jsx
+++ src/COMPONENTS/homeComponents/homeComponent.jsx
@@ -241,6 +241,12 @@ const HomePageComponent = ({ eventsArray }) => {
 
   return (
     <div className={classes.homePageComponentContainer}>
+      <EventsImagesWheel eventsArray={eventsArray} />
+      <PopularArtistsNear
+        interestedArtists={interestedArtists}
+        setInterestedArtists={setInterestedArtists}
+        data={eventsArray}
+      />
       {interestedArtists.length > 0 && (
         <PopularArtistsNear
           data={interestedArtists}
@@ -248,12 +254,6 @@ const HomePageComponent = ({ eventsArray }) => {
           setInterestedArtists={setInterestedArtists}
         />
       )}
-      <EventsImagesWheel eventsArray={eventsArray} />
-      <PopularArtistsNear
-        interestedArtists={interestedArtists}
-        setInterestedArtists={setInterestedArtists}
-        data={eventsArray}
-      />
     </div>
   );
 };
